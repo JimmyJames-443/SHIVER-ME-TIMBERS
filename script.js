00e81f49f99c33459846164fb1f91eb27eda5d77
James Ndirangu <jimmy443ndirangumuriithi@gmail.com>
2026-02-09 11:09:30 +0300

    confetti and some js shananigans

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const hullInfo = document.getElementById('hull info');
 button.addEventListener('click', () => {
     hullInfo.textContent = "Within the ship are hidden treasures found from the lost island of JUTSU!";
 });
+
+const holla = document.getElementById('tbutton');
+holla.addEventListener('click', () =>{
+console.log("user wants to send a message asap");
+confetti({
+    particleCount: 150,
+    spread: 70,
+    origin: { y: 0.6 }
+})
+
+});
